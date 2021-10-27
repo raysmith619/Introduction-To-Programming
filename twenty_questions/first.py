545f0b9c36d99867896dc1096435121c9562c8e1
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2021-10-27 11:48:18 -0400

    twenty_questions moved to exercises
    Moved to exercises/twenty_questions_dev

--- twenty_questions/first.py
+++
@@ -1,7 +0,0 @@
-# first 17Jul2018
-# loop asking number
-# print number entered
-while True:
-    inp = input("Enter Number ")
-    num = int(inp)
-    print("Number:", num)
--- twenty_questions/snd.py
+++
@@ -1,7 +0,0 @@
-# second.py 17Jul2018
-# loop asking number
-# print number entered
-while True:
-    inp = input("Enter Number ")
-    num = int(inp)
-    print("Number:", num)
