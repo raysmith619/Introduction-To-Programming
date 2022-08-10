812cac75ae70e7bdd1e5798924972b84cf3ae476
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2022-08-10 16:51:12 -0400

    Add files via upload

--- exercises/twenty_questions_dev/iteration_2.py
+++ exercises/twenty_questions_dev/iteration_2.py
@@ -1,9 +1,12 @@
-# iteration_2.py 21Jan2020
-# first iteration of twenty questions
-# + Prompt user for number
-#
-# + Set up target, check for match
-# Use integer values
+# iteration_2.py 21Jan2020 crs, from iteration_1.py
+"""
+Second iteration of twenty questions
+
+ + Prompt user for number
+
+ + Set up target, check for match
+ Use integer values
+"""
 
 target = 15
 while True:
@@ -11,7 +14,7 @@ while True:
     print("Number:", inp)
     guess = int(inp)    # Convert to integer
     if guess == target:
-        print("Congratulations", guess, "the target")
+        print("Congratulations", guess, "is the target")
         break
 
     
